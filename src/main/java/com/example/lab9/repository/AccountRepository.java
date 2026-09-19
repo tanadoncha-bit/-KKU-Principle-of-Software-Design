eeb1d09d1503cbce73be0ac57bdfaad6356ade2b
tanadoncha-bit <tanadon.cha@kkumail.com>
2026-09-19 20:35:07 +0700

    add Lab10

--- src/main/java/com/example/lab9/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab9.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.lab9.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account,Long>{
-
-}
--- src/main/java/com/example/lab9/repository/DepositRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab9.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.lab9.model.DepositTransaction;
-
-public interface DepositRepository extends JpaRepository<DepositTransaction,Long>{
-    
-}
